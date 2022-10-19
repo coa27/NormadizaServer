7c17895240b56a46d3c432178f174dddc28a880b
Desktop <coa27-00@hotmail.com>
2022-10-19 06:04:05 -0500

    commit inicial

---
+++ src/test/java/com/coa/NormadizaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormadizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
